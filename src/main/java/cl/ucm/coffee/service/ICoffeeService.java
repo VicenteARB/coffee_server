a93f45fa6d08baf9e5412e858c3f6626c70ab667
Crlsljndr <carlosalejandro-@hotmail.cl>
2024-06-22 23:52:51 -0400

    22-06

--- src/main/java/cl/ucm/coffee/service/ICoffeeService.java
+++ src/main/java/cl/ucm/coffee/service/ICoffeeService.java
@@ -3,9 +3,14 @@ package cl.ucm.coffee.service;
 import cl.ucm.coffee.persitence.entity.CoffeeEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ICoffeeService {
 
     List<CoffeeEntity> getCoffeeList();
     CoffeeEntity save(CoffeeEntity coffeeEntity);
+
+    Optional<CoffeeEntity> findByName(String name);
+    CoffeeEntity updateCoffee(CoffeeEntity coffeeEntity);
+    void deleteCoffee(int idCoffee);
 }
